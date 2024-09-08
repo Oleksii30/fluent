637ec0868d3c4691f5219fb6a8c1cd75ef3c2884
Oleksii30 <polyanovskyyol@gmail.com>
2024-09-08 20:28:48 +0300

    mobile styles for main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { GetServerSidePropsContext } from "next";
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -6,12 +7,24 @@ import Header from 'components/header';
 import { useAuthState } from 'context/auth';
 import { Routes } from 'enums/routes';
 import MainButton from 'components/buttons/main';
+import { useIsServerSideMobile } from 'context/serverSideMobile';
+import { getIsSsrMobile } from 'helpers/serverSideMobile';
 
 import styles from 'styles/pages/Home.module.css';
 
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  return {
+    props: {
+      isSsrMobile: true //getIsSsrMobile(context)
+    }
+  };
+}
+
 export default function Home() {
   const { isLoggedIn, user } = useAuthState();
-  const router = useRouter()
+  const router = useRouter();
+
+  const isTabletOrMobile = useIsServerSideMobile();
 
   const handleCickAction = () => {
     const route = isLoggedIn ? Routes.SIGNUP : Routes.CREATE;
@@ -27,39 +40,37 @@ export default function Home() {
       </Head>
       <Header/>
       <main className={styles.main}>
-        <div style={{display: 'flex'}}>
+        <div className={styles.section}>
           <Image
             style={{borderTopLeftRadius: 50, borderBottomRightRadius: 50}}
             src='/images/f_1.png' alt='f_1'
-            width={502}
-            height={381}
+            width={isTabletOrMobile ? 350 : 502}
+            height={isTabletOrMobile ? 269 : 381}
           />
-          <div className={styles.section}>
-            <div style={{width:'60%', textAlign:'center'}}>
-              <span className={styles.big_text}>
-                Fluently is your helper in
-                learning languages
-              </span>
-              <div style={{marginTop: 20}}>
-                <MainButton label={isLoggedIn ? 'Create List' : 'Sign Up'} type='button' onClick={handleCickAction}/>
-              </div>
+          <div style={{width: isTabletOrMobile ? '100%': '60%', textAlign:'center'}}>
+            <span className={isTabletOrMobile ? styles.big_text_mobile : styles.big_text}>
+              Fluently is your helper in
+              learning languages
+            </span>
+            <div style={{marginTop: 20}}>
+              <MainButton label={isLoggedIn ? 'Create List' : 'Sign Up'} type='button' onClick={handleCickAction}/>
             </div>
           </div>
         </div>
-        <div style={{padding: 70}}>
-          <span className={styles.semi_big_text}>
+        <div style={{padding: isTabletOrMobile ? 10 : 70, textAlign:'center'}}>
+          <span className={isTabletOrMobile ? styles.semi_big_text_mobile : styles.semi_big_text} >
             It works by creating lists of words
           </span>
         </div>
-        <div className={styles.section}>
-          <div style={{width:'40%', textAlign:'center'}}>
-            <span className={styles.semi_big_text}>
+        <div className={styles.section} style={{flexWrap: isTabletOrMobile ? 'wrap-reverse' : 'wrap'}}>
+          <div style={{width: isTabletOrMobile ? '100%': '40%', textAlign:'center'}}>
+            <span className={isTabletOrMobile ? styles.semi_big_text_mobile : styles.semi_big_text}>
               Watching a video or a film
               and found  something that will
               enrich your language
             </span>
             <div style={{marginTop:40}}>
-              <span className={styles.big_text_red}>
+              <span className={isTabletOrMobile ? styles.big_text_red_mobile : styles.big_text_red}>
                 Add it to your List
               </span>
             </div>
@@ -67,43 +78,43 @@ export default function Home() {
           <Image
             style={{borderRadius: 50}}
             src='/images/f_2.png' alt='f_2'
-            width={503}
-            height={250}
+            width={isTabletOrMobile ? 350 : 503}
+            height={isTabletOrMobile ? 175 : 250}
           />
         </div>
         <div className={styles.section}>
           <Image
             style={{borderTopRightRadius: 50, borderBottomLeftRadius: 50}}
             src='/images/f_3.png' alt='f_3'
-            width={489}
-            height={331}
+            width={isTabletOrMobile ? 350 : 489}
+            height={isTabletOrMobile ? 236 : 331}
           />
-          <div style={{width:'45%', textAlign:'center', paddingTop:20}}>
-            <span className={styles.semi_big_text}>
+          <div style={{width: isTabletOrMobile ? '100%': '45%', textAlign:'center', paddingTop:20}}>
+            <span className={isTabletOrMobile ? styles.semi_big_text_mobile : styles.semi_big_text}>
               Reading a book and
               encountered a word that
               sounds really sophisticating
             </span>
-            <div style={{marginTop:40}}>
-              <span className={styles.big_text_red}>
+            <div style={{marginTop:40, textAlign:'center'}}>
+              <span className={isTabletOrMobile ? styles.big_text_red_mobile : styles.big_text_red}>
                 Add it to your List
               </span>
             </div>
           </div>
         </div>
-        <div style={{padding:30}}>
-          <span className={styles.semi_big_text}>
+        <div style={{padding:30, textAlign:'center'}}>
+          <span className={isTabletOrMobile ? styles.semi_big_text_mobile : styles.semi_big_text}>
             You are struggling to understand what`s your favourite song is about
           </span>
         </div>
         <Image
           style={{borderRadius: '50%'}}
           src='/images/f_4.png' alt='f_4'
-          width={406}
-          height={390}
+          width={isTabletOrMobile ? 300 : 406}
+          height={isTabletOrMobile ? 290 : 390}
         />
         <div style={{padding:30}}>
-          <span className={styles.semi_big_text}>
+          <span className={isTabletOrMobile ? styles.semi_big_text_mobile : styles.semi_big_text}>
             You know what to do
           </span>
         </div>
