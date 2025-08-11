5c94b5842fba1bb7be60043d1f1802256f29049f
Oleksii30 <polyanovskyyol@gmail.com>
2025-08-11 11:26:00 +0300

    changed loading screen to spinner

--- components/savingScreen.tsx
+++ components/savingScreen.tsx
@@ -1,8 +1,19 @@
+import { Triangle } from 'react-loader-spinner';
 import styles from 'styles/components/SavingScreen.module.css';
 
 export default function SavingScreen() {
 
 	return (
-		<div className={styles.screen_container}></div>
+		<div className={styles.screen_container}>
+			<Triangle
+				visible={true}
+				height="70"
+				width="70"
+				color="black"
+				ariaLabel="triangle-loading"
+				wrapperStyle={{}}
+				wrapperClass=""
+			/>
+		</div>
 	)
 }
